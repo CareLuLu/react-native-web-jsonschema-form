442e63fba8e2301b3c1540219c96ce386ad6ac3e
Archimedes Trajano <developer@trajano.net>
2021-06-23 12:14:28 -0400

    Initial release
    Initial cut for now.  This is to provide some typings for what I am using in my project.
    
    https://github.com/CareLuLu/react-native-web-jsonschema-form/issues/256
    
    Don't merge yet. but you may expand on it if you wish.

---
+++ src/index.d.ts
@@ -0,0 +1,36 @@
+declare module "react-native-web-jsonschema-form" {
+  export interface FormEvent {
+    name: string;
+    prevented: boolean;
+  }
+  export interface FormChangeEvent<T> extends FormEvent {
+    name: 'change';
+    params: {
+      metas: Record<string, unknown>;
+      name: string;
+      value: unknown;
+      /**
+       * Original values before the change.
+       */
+      values: T;
+    };
+  }
+  export interface FormSubmitEvent<T> extends FormEvent {
+    name: 'submit';
+    params: {
+      values: T;
+    };
+  }
+
+  export declare function Form<T extends Record<string,unknown>>(props: {
+    formData: T,
+    schema: Record<string,unknown>,
+    uiSchema: Record<string,unknown>,
+    widgets: Record<string, JSX.Element>,
+    HeadingComponent: JSX.Element,
+    SubmitButton: JSX.Element,
+    onChange: (event: FormChangeEvent<T>) => Promise<void>
+    onSubmit: (event: FormSubmitEvent<T>) => Promise<void>
+  }): JSX.Element;
+
+}
