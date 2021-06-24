3fa050455793ca18ca00989863caed4be2f3e7ba
Archimedes Trajano <developer@trajano.net>
2021-06-24 15:02:59 -0400

    Added the props

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,12 @@
+// @ts-check
+import { ReactChild } from "react";
+
 declare module "react-native-web-jsonschema-form" {
   type BaseFormData = Record<string, unknown>;
   type JsonSchema = Record<string, unknown>;
-  type Uichema = Record<string, unknown>;
+  type UiSchema = Record<string, unknown>;
+  type MetaSchema = Record<string, unknown>;
+  type ErrorSchema = Record<string, unknown>;
   export class FormEvent {
     name: string;
     params: Record<string, unknown>;
@@ -32,16 +37,33 @@ declare module "react-native-web-jsonschema-form" {
     };
   }
 
-  export declare function Form<T extends BaseFormData>(props: {
-    formData: T;
-    schema: JsonSchema;
-    uiSchema: UiSchema;
-    widgets: Record<string, (props: any) => JSX.Element>;
-    HeadingComponent: (props: any) => JSX.Element;
-    SubmitButton: (props: any) => JSX.Element;
-    onChange: (event: FormChangeEvent<T>) => Promise<void>;
-    onSubmit: (event: FormSubmitEvent<T>) => Promise<void>;
-  }): JSX.Element;
+  interface JsonSchemaFormProps<T extends BaseFormData> {
+    name?: string;
+    schema?: JsonSchema;
+    uiSchema?: UiSchema;
+    metaSchema?: MetaSchema;
+    errorSchema?: ErrorSchema;
+    formData?: T;
+    children?: ReactChild | ReactChild[];
+    onRef?: () => {};
+    onChange?: (event: FormChangeEvent<T>) => Promise<void>;
+    onSubmit?: (event: FormSubmitEvent<T>) => Promise<void>;
+    onCancel?: (event: FormEvent) => Promise<void>;
+    onSuccess?: (event: FormEvent) => Promise<void>;
+    onError?: (event: FormEvent) => Promise<void>;
+    buttonPosition?: "left" | "right" | "center";
+    cancelButton?: bool | string;
+    CancelButton?: JSX.Element;
+    submitButton?: bool | string;
+    SubmitButton?: JSX.Element;
+    scroller?: Record<string, unknown>;
+    widgets?: Record<string, (props: any) => JSX.Element>;
+    filterEmptyValues?: boolean;
+    insideClickRegex?: RegExp;
+  }
+  declare function JsonSchemaForm<T extends BaseFormData>(
+    props: JsonSchemaFormProps<T>
+  ): JSX.Element;
 
-  export default Form;
+  export default JsonSchemaForm;
 }
